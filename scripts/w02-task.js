20dcadb79ec0ee2910a2185f245472b7b70b7327
AOPeaco <Andrew.o.peacock@gmail.com>
2024-01-19 18:20:55 -0700

    Fixed the issues with some help.

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,23 +4,23 @@
 
 /* Step 2 - Variables */
 
-let fullName = "Andrew Peacock";
-let currentYear = new Date();
-let profilePicture = 'C:\Users\Andre\OneDrive\Documents\BYUI Winter 2024\cse121b\images\image0 (8).jpeg';
+let fullName = 'Andrew Peacock';
+let currentYear = new Date().getFullYear();
+let profilePicture = 'images/selfPortrait.png';
 
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.getElementById("img");
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
-imageElement.setAttribute("src", profilePicture);
-imageElement.setAttribute('alt', `profile image of ${fullName}`);
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image for ${fullName}`);
 
 /* Step 5 - Array */
 
@@ -29,6 +29,6 @@ foodElement.innerHTML = `${favFood}`;
 let otherFood = favFood.push('Steak');
 foodElement.innerHTML += `<br>${favFood}`;
 let firstFood = favFood.shift();
-foodElement.innerHTML += `${favFood}`;
+foodElement.innerHTML += `<br>${favFood}`;
 let lastFood = favFood.pop();
-foodElement.innerHTML += `${favFood}`;
+foodElement.innerHTML += `<br>${favFood}`;
